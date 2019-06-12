10b621441075f6c5ffb0098d60f527b716c15e74
Crowdbotics <team@crowdbotics.com>
2019-06-12 03:22:06 +0000

    initial commit

---
+++ backend/angeliques_beauty_su_4556/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for angeliques_beauty_su_4556 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'angeliques_beauty_su_4556.settings')
+
+application = get_wsgi_application()
